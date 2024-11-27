80a00e791475a889a00559831eaa20481b05a062
Hamsterbil <gunnarmani04@gmail.com>
2024-11-27 17:07:24 +0100

    Everything combined
    Needs a lot of touchup

--- Project1LandmineGrid/cmake-build-debug/cmake_install.cmake
+++ Project1LandmineGrid/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Patrick Brendstrup/CLionProjects/Project1LandmineGrid
+# Install script for directory: C:/Users/gunna/Documents/Datalogi/P1/P1-Landmine/Project1LandmineGrid
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Patrick Brendstrup/CLionProjects/Project1LandmineGrid/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/gunna/Documents/Datalogi/P1/P1-Landmine/Project1LandmineGrid/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
