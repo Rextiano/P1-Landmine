e4cf6c7302801d229900b649a63f74835876c9a2
Hamsterbil <gunnarmani04@gmail.com>
2024-11-29 14:39:38 +0100

    Print path

--- Project1LandmineGrid/src/dijkstra.c
+++ Project1LandmineGrid/src/dijkstra.c
@@ -1,7 +1,8 @@
 #include "functions.h"
 #include <math.h>
+#include <stdio.h>
 
-void findMandatoryNodes(int grid[GRID_SIZE][GRID_SIZE], Node nodes[GRID_SIZE * GRID_SIZE + 1])
+void findMandatoryNodes(int l, int w, Node grid[l][w], Node nodes[l * w + 1], int threshold)
 {
 
 }
@@ -11,9 +12,9 @@ void nearestNeighbor(Node *nodes, int count, int *order)
 
 }
 
-int isValid(int x, int y)
+int isValid(int x, int y, int l, int w)
 {
-    return x >= 0 && x <= GRID_SIZE && y >= 0 && y <= GRID_SIZE;
+    return x >= 0 && x <= w && y >= 0 && y <= l;
 }
 
 int distance(Node a, Node b)
@@ -21,12 +22,19 @@ int distance(Node a, Node b)
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
-void dijkstra(Node ref, Node tar, int risk[GRID_SIZE][GRID_SIZE], Node parent[GRID_SIZE][GRID_SIZE], int grid[GRID_SIZE][GRID_SIZE])
+void dijkstra(int l, int w, Node ref, Node tar, Node grid[l][w])
 {
 
 }
 
-void printPath(Node prev[GRID_SIZE][GRID_SIZE], Node target)
+void printPath(int l, int w, Node grid[l][w], Node current)
 {
+    if ((grid[current.x][current.y].parent[0] < 0 && grid[current.x][current.y].parent[1] < 0) ||
+        (grid[current.x][current.y].parent[0] == current.x && grid[current.x][current.y].parent[1] == current.y)) {
+        printf("(%d, %d)", current.x, current.y);
+        return;
+        }
 
+    printf("(%d, %d) -> ", current.x, current.y);
+    printPath(l, w, grid, grid[current.parent[0]][current.parent[1]]);
 }
--- Project1LandmineGrid/src/functions.h
+++ Project1LandmineGrid/src/functions.h
@@ -1,13 +1,12 @@
-#define GRID_SIZE 5   // Grid Size. Only a square for now
-#define THRESHOLD 80  // The determined threshold a node should be above or equal to
-
 typedef struct { //Struct to better find positions of nodes
     int x, y;
+    int risk;
+    int parent[2];
 } Node;
 
-void findMandatoryNodes(int grid[GRID_SIZE][GRID_SIZE], Node nodes[GRID_SIZE * GRID_SIZE + 1]); //Should update a list of mandatory nodes (coordinates with values above threshold)
+void findMandatoryNodes(int l, int w, Node grid[l][w], Node nodes[l * w + 1], int threshold); //Should update a list of mandatory nodes (coordinates with values above threshold)
 void nearestNeighbor(Node *nodes, int count, int *order); //Algorithm to order each mandatory node found by closest coordinates
-int isValid(int x, int y); //Check if coordinate is within grid
+int isValid(int x, int y, int l, int w); //Check if coordinate is within grid
 int distance(Node a, Node b); //Find distance between two nodes' coordinates to determine the closest
-void dijkstra(Node ref, Node tar, int risk[GRID_SIZE][GRID_SIZE], Node parent[GRID_SIZE][GRID_SIZE], int grid[GRID_SIZE][GRID_SIZE]); //Actual algorithm to determine best route from source to target
-void printPath(Node parent[GRID_SIZE][GRID_SIZE], Node target); //Print the result to debug
+void dijkstra(int l, int w, Node ref, Node tar, Node grid[l][w]); //Actual algorithm to determine best route from source to target
+void printPath(int l, int w, Node grid[l][w], Node current); //Print the result to debug
