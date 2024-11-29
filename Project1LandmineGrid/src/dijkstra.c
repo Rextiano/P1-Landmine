fa8267da9e1dad483b75589e4eceaaeb71cd31f9
Hamsterbil <gunnarmani04@gmail.com>
2024-11-29 13:15:47 +0100

    Added isValid and distance

--- Project1LandmineGrid/src/dijkstra.c
+++ Project1LandmineGrid/src/dijkstra.c
@@ -1,4 +1,5 @@
 #include "functions.h"
+#include <math.h>
 
 void findMandatoryNodes(int grid[GRID_SIZE][GRID_SIZE], Node nodes[GRID_SIZE * GRID_SIZE + 1])
 {
@@ -12,12 +13,12 @@ void nearestNeighbor(Node *nodes, int count, int *order)
 
 int isValid(int x, int y)
 {
-
+    return x >= 0 && x <= GRID_SIZE && y >= 0 && y <= GRID_SIZE;
 }
 
 int distance(Node a, Node b)
 {
-
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
 void dijkstra(Node ref, Node tar, int risk[GRID_SIZE][GRID_SIZE], Node parent[GRID_SIZE][GRID_SIZE], int grid[GRID_SIZE][GRID_SIZE])
